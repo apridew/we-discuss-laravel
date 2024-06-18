7ae7b8a3a58ea45ff7fefcd7c5c2d301557342a7
apridew <dewiapriyantiputra@gmail.com>
2024-06-18 21:05:00 +0700

    [ADD] auth logout

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -23,6 +23,32 @@
                 <input class="form-control border-start-0 ps-0" type="search" placeholder="Search" aria-label="Search" name="" value="">
               </div>
             </form>
+            @auth
+                <ul class="navbar-nav ms-auto my-3 my-lg-0">
+                  <li class="nav-item my-auto dropdown">
+                    <a class="nav-link p-0 d-flex align-items-center" href="javascript:;" data-bs-toggle="dropdown">
+                      <div class="me-2">
+                        <img src="{{ filter_var(auth()->user()->picture, FILTER_VALIDATE_URL) 
+                          ? auth()->user()->picture : Storage::url(auth()->user()->picture) }}" 
+                          alt="{{ auth()->user()->username }}" class="avatar rounded-circle">
+                      </div>
+                      <span class="fw-bold">{{ auth()->user()->username }}</span>
+                    </a>
+                    <ul class="dropdown-menu mt-2">
+                      <li>
+                        <a class="dropdown-item" href="{{ route('users.show', auth()->user()->username) }}">My Profile</a>
+                      </li>
+                      <li>
+                        <form action="{{ route('auth.login.logout')}}" method="POST">
+                          @csrf
+                          <button type="submit" class="dropdown-item">Log out</button>
+                        </form>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+            @endauth
+            @guest               
             <div class="d-flex flex-column flex-lg-row gap-2 ms-2 mt-2 mt-lg-0 ">
               <button class="btn btn-primary ps-1 pe-0">
                 <a class="nav-link text-nowrap" href="{{route("auth.login.show")}}">Log In</a>
@@ -31,6 +57,7 @@
                 <a class="nav-link text-nowra" href="{{route("auth.signup.show")}}">Sign Up</a>
               </button>
             </div>
+            @endguest
           </div>
         </div>
       </nav>
