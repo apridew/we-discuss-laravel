103099bcaaa4e3b5e0a0265fc4a3051c0ccfdbdc
apridew <dewiapriyantiputra@gmail.com>
2024-06-18 20:25:42 +0700

    [ADD] login request

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -7,18 +7,28 @@
                 <div class="col-12 col-lg-3">
                     <a href="{{route('home')}}" class="nav-link mb-4 text-center fs-3 text-primary fw-bold spa">WeDiscuss</a>
                     <div class="card mb-4">
-                        <form action="">
+                        <form action="{{route('auth.login.login')}}" method="POST">
+                            @csrf
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="email" placeholder="name@example.com" autocomplete="off" autofocus>
+                                <input name="email" type="email" class="form-control @error('email') is-invalid @enderror @error('credentials') is-invalid @enderror" id="email" placeholder="name@example.com" autocomplete="off" value="{{old('email')}}" autofocus>
+                                @error('email')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                                @error('credentials')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
                             <div class="mb-4">
                                 <label for="password" class="form-label">Password</label>
                                 <div class="input-group mb-3">
-                                    <input id="password" name="password" type="password" class="form-control" placeholder="password" aria-label="Password" aria-describedby="password">
-                                    <span class="input-group-text" id="password">
+                                    <input id="password" name="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="password" aria-label="Password" aria-describedby="password">
+                                    <span class="input-group-text @error('password') border-danger rounded-end @enderror" id="password">
                                         <a href="javascript:;" id="password-toggle"><img id="password-toggle-img" src="{{url('assets/img/eye-slash.png')}}" alt="password"></a>
                                     </span>
+                                    @error('password')
+                                       <div class="invalid-feedback">{{ $message }}</div>   
+                                    @enderror
                                 </div>
                             </div>
                             <div class="mb-3 d-grid">
