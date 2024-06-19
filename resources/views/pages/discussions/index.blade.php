58146a3d5edcb02efe516adbf28501f4b8c95473
apridew <dewiapriyantiputra@gmail.com>
2024-06-19 15:17:41 +0700

    [ADD] pagination

--- resources/views/pages/discussions/index.blade.php
+++ resources/views/pages/discussions/index.blade.php
@@ -43,7 +43,7 @@
                                     <a href="{{route('discussions.show', $discussion->slug)}}">
                                         <h3>{{$discussion->title}}</h3>
                                     </a>
-                                    <p>{{$discussion->content_preview}}</p>
+                                    <p>{!!$discussion->content_preview!!}</p>
                                     <div class="row">
                                         <div class="col me-1 me-lg-2">
                                             <a href="">
@@ -71,6 +71,8 @@
                                 Currently no discussion yet
                             </div>
                         @endforelse
+
+                        {{$discussions->links()}}
                     </div>
                     <div class="col-12 col-lg-4">
                         <div class="card">
